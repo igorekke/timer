d75fe4e7d63b19f24a8e23bcc09a7cb00b0f6b07
igorekke <113307897+igorekke@users.noreply.github.com>
2022-09-28 18:08:26 +0200

    Add files via upload

---
+++ timer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	for (int i=10; i>=0; i--)
+	{
+		Sleep(1000);
+		system("cls");
+		cout << i << endl;
+	}
+	cout << "Boom" << endl;
+}
